24c345b2b67c259df9b7ac5c1d8e181979826f65
Asier González <asigonlo@gmail.com>
2016-09-20 21:54:24 +0200

    Adding loadScript()

--- helper.js
+++ helper.js
@@ -37,22 +37,32 @@ function httpRequest(httpObj, path) {
  	httpObj.send(null);
 }
 
-/* AJAX TEMPLATE
+function loadScript(url, callback) {
+	// Adding the script tag to the head as suggested before
+	var head = document.getElementsByTagName('head')[0];
+	var script = document.createElement('script');
+	script.type = 'text/javascript';
+	script.src = url;
 
+	// Then bind the event to the callback function.
+	script.onreadystatechange = callback;
+	script.onload = callback;
+
+	// Fire the loading
+	head.appendChild(script);
+}
+
+/* AJAX TEMPLATE
 function loadAJAX() {
 	var httpObj = createHttp();
 
 	httpObj.onreadystatechange = function () {
 		if (httpObj.readyState == 4 && httpObj.status == 200) {
-
 			// CODE
-
 		}
 	};
-
 	httpRequest(httpObj, FILEPATH);
 }
-
 */
 
 ///////////////////////////////////////////////////////////////////////////////
